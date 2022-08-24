bbd907c94be52905cc23bf16e63730e7a14ded65
richinrix <richinvishal@gmail.com>
2022-08-24 20:27:30 +0530

    removing unused modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-const url = "https://dummydata-fqqi0aufx-richinrix.vercel.app/data.json";
-const clientID =
-  "178417619857-2dr5pf2vgpgq31c05s4rqp880761f024.apps.googleusercontent.com";
-const clientSecret = "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";
+
 function App(props) {
   const { user } = props;
   return (
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./userSclice";
-
-export default configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
